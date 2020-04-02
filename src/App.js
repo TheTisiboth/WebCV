8136bbf8348923037f122b50e2fe1bdc5541d98f
thetisiboth <thetisiboth@gmail.com>
2020-04-02 19:35:14 +0200

    Minor fixes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,62 @@ import React, { Component, Suspense } from 'react';
 import { useTranslation, withTranslation, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, NavDropdown,Button } from 'react-bootstrap'
+
+/**
+ * Translation button, that translate the whole page. It switches between english (by default) and french
+ */
+class TranslationButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isEnglish: true};
+    this.buttonLabel = 'en'; 
+    this.changeLanguage = lng => { // Change the language of the whole page
+      this.props.i18n.changeLanguage(lng);
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const isEnglish = this.state.isEnglish ? false : true;
+    this.buttonLabel = (isEnglish) ? 'en' : 'fr'
+    this.changeLanguage(this.buttonLabel)
+    this.setState({ isEnglish: isEnglish })
+  }
+
+  render() {
+    return (
+      <Button variant="outline-light" onClick={this.handleClick}>{this.buttonLabel}</Button>
+    );
+  }
+}
+
+const _TranslationButton = withTranslation()(TranslationButton);
+
+function MyNavbar() {
+  return (
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Léo Jan</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Présentation</Nav.Link>
+          <Nav.Link href="#pricing">Scolarité</Nav.Link>
+          <NavDropdown title="Compétences" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <_TranslationButton />
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
 // use hoc for class based components
 class LegacyWelcomeClass extends Component {
@@ -15,9 +71,8 @@ const Welcome = withTranslation()(LegacyWelcomeClass);
 // Component using the Trans component
 function MyComponent() {
   return (
-    <Trans i18nKey="description.part1">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </Trans>
+    <Trans i18nKey="description.part1" />
+
   );
 }
 
@@ -25,23 +80,19 @@ function MyComponent() {
 function Page() {
   const { t, i18n } = useTranslation();
 
-  const changeLanguage = lng => {
-    i18n.changeLanguage(lng);
-  };
-
   return (
     <div className="App">
+      <MyNavbar />
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <Welcome />
-        <button onClick={() => changeLanguage('de')}>de</button>
-        <button onClick={() => changeLanguage('en')}>en</button>
-        <button onClick={() => changeLanguage('fr')}>fr</button>
+
       </div>
       <div className="App-intro">
         <MyComponent />
       </div>
-      <div>{t('description.part2')}</div>
+      {/* <div>{t('description.part2')}</div> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import i18n (needs to be bundled ;))
 import './i18n';
