d81638263433430eac753c73a08629d516cd8ce4
thetisiboth <thetisiboth@gmail.com>
2020-04-08 22:46:44 +0200

    Dynamic map

--- src/App.tsx
+++ src/App.tsx
@@ -408,6 +408,7 @@ function AppHeader() {
 }
 
 function Travel() {
+
   const { t } = useTranslation();
   const positions = [43.3, 5.4];
   const icon = new Icon({
@@ -425,7 +426,7 @@ function Travel() {
     return s;
   }
 
-  const country =
+  const countries =
     [
       {
         // germany
@@ -539,6 +540,157 @@ function Travel() {
       },
     ];
 
+  const cities =
+    [
+      {
+        lat: 48.13825988769531,
+        lon: 11.584508895874023,
+        tooltip: t('travel.germany.munich')
+      },
+      {
+        lat: 52.51763153076172,
+        lon: 13.40965747833252,
+        tooltip: t('travel.germany.berlin')
+      },
+      {
+        // germany
+        lat: 53.56729507446289,
+        lon: 9.941673278808594,
+        tooltip: t('travel.germany.hamburg')
+      },
+      {
+        // germany
+        lat: 51.960906982421875,
+        lon: 7.628866195678711,
+        tooltip: t('travel.germany.munster')
+      },
+      {
+        // tchequie
+        lat: 50.05708312988281,
+        lon: 14.44813060760498,
+        tooltip: t('travel.tchequie.prague')
+      },
+      {
+        // belgium
+        lat: 50.6451381,
+        lon: 5.5734203,
+        tooltip: t('travel.belgium.liege')
+      },
+      {
+        // belgium
+        lat: 50.8436709,
+        lon: 4.3674367,
+        tooltip: t('travel.belgium.brussels')
+      },
+      {
+        // canada
+        lat: 45.509063720703125,
+        lon: -73.55335998535156,
+        tooltip: t('travel.canada.montreal')
+      },
+      {
+        // canada
+        lat: 46.825754, 
+        lon: -71.208490,
+        tooltip: t('travel.canada.quebec')
+      },
+      {
+        // canada
+        lat: 48.1433429,
+        lon: -69.7174574,
+        tooltip: t('travel.canada.tadoussac')
+      },
+      {
+        // canada
+        lat: 45.40139389038086,
+        lon: -71.89034271240234,
+        tooltip: t('travel.canada.sherbrooke')
+      },
+      {
+        // spain
+        lat: 39.61391960090061,
+        lon: 2.9746341705322266,
+        tooltip: t('travel.spain.palma')
+      },
+      {
+        // spain
+        lat: 41.39219284057617,
+        lon: 2.164867401123047,
+        tooltip: t('travel.spain.barcelona')
+      },
+      {
+        // italy
+        lat: 41.8892936706543,
+        lon: 12.493546485900879,
+        tooltip: t('travel.italy.roma')
+      },
+      {
+        // italy
+        lat: 40.83998489379883,
+        lon: 14.252542495727539,
+        tooltip: t('travel.italy.naples')
+      },
+      {
+        // italy
+        lat: 40.74886703491211,
+        lon: 14.501250267028809,
+        tooltip: t('travel.italy.pompei')
+      },
+      {
+        // greece
+        lat: 37.99076843261719,
+        lon: 23.74122428894043,
+        tooltip: t('travel.greece.athens'),
+      },
+      {
+        // greece
+        lat: 37.938621520996094,
+        lon: 22.92695426940918,
+        tooltip: t('travel.greece.corinth'),
+      },
+      {
+        // us
+        lat: 42.92121887207031,
+        lon: -75.62081909179688,
+        tooltip: t('travel.us.ny')
+      },
+      {
+        // uk
+        lat: 52.865196,
+        lon: -7.9794599,
+        tooltip: t('travel.uk.ireland')
+      },
+      {
+        // fr
+        lat: 43.2961743,
+        lon: 5.3699525,
+        tooltip: t('travel.fr.marseille')
+      },
+      {
+        // sweden
+        lat: 59.32796859741211,
+        lon: 18.05364227294922,
+        tooltip: t('travel.sweden.stockholm')
+      },
+    ];
+  const [state, setState] = useState({
+    markers: countries
+  });
+
+  function onZoom(e: any) {
+    const zoom = e.target._zoom;
+    console.log(zoom);
+    if (zoom >= 6) {
+      setState({
+        markers : cities
+      })
+    } else {
+      setState({
+        markers : countries
+      })
+    }
+  }
+
   return (
     <Container id="Travels">
       <Row className="mb-4">
@@ -555,11 +707,11 @@ function Travel() {
       </Row>
       <Row>
         <Col>
-          <Map style={{ width: '100%', height: '500px' }} center={[54.370138916189596, -29.918133437500003]} zoom={3}>
+          <Map style={{ width: '100%', height: '500px' }} center={[54.370138916189596, -29.918133437500003]} zoom={3} onZoomend={onZoom}>
             <TileLayer
               url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
             />
-            {country.map(c => {
+            {state.markers.map(c => {
               return <Marker key={c.tooltip} position={[c.lat, c.lon]}>
                 <TooltipL>{c.tooltip}</TooltipL>
               </Marker>
