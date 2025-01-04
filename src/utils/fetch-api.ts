12a48657e453fc5000a21a12fb757ac1a5c84747
Leo Jan <leo.jan@doctolib.com>
2025-01-04 12:44:47 +0100

    Sort fetchAPI by id in ascending order

--- src/utils/fetch-api.ts
+++ src/utils/fetch-api.ts
@@ -1,18 +1,14 @@
 export async function fetchAPI<T>(resource = '', locale = 'en') {
     const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN
     const options = {headers: {Authorization: `Bearer ${token}`}}
-    const params = new URLSearchParams({populate: '*', locale})
+    const params = new URLSearchParams({populate: '*', locale, 'sort': 'id:asc'})
 
     const url = new URL(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/${resource}?${params}`)
 
-    try {
-        const response = await fetch(url, options)
-        if (!response.ok) {
-            throw new Error(`Failed to fetch data from Strapi (url=${url}, status=${response.status})`)
-        }
-        return (await response.json()).data as T
-    } catch (error) {
-        console.error(error)
-        throw error
+    const response = await fetch(url, options)
+    if (!response.ok) {
+        console.log(await response.text())
+        throw new Error(`Failed to fetch data from Strapi (url=${url}, status=${response.status})`)
     }
+    return (await response.json()).data as T
 }
