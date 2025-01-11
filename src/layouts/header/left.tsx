a9b67ba639f7b0da846d8cd116a1799637a4163a
Leo Jan <leo.jan@doctolib.com>
2025-01-11 15:14:26 +0100

    Fix code style

--- src/layouts/header/left.tsx
+++ src/layouts/header/left.tsx
@@ -2,7 +2,7 @@ import {FC} from 'react'
 import {Button, Col, Row} from 'react-bootstrap'
 import {computeAge} from '../../utils'
 import {useTranslations} from 'next-intl'
-import Link from "next/link";
+import Link from 'next/link'
 
 /**
  * LeftHeader, containing general info about author
@@ -12,8 +12,8 @@ const LeftHeader: FC = () => {
     return (
         <div>
             <Row>
-                <Col md={12} className="pr-0 pl-0">
-                    <div className="name">
+                <Col md={12} className='pr-0 pl-0'>
+                    <div className='name'>
                         <h1>Léo Jan</h1>
                     </div>
                     <div>
@@ -23,16 +23,16 @@ const LeftHeader: FC = () => {
                         <h5>{computeAge()} {t('years')}</h5>
                     </div>
                 </Col>
-                <Col md={12} className="align-self-center pt-3 pt-md-0 pb-3 pb-md-0 mt-md-5 pr-0 pl-0">
-                    <div className="name">
+                <Col md={12} className='align-self-center pt-3 pt-md-0 pb-3 pb-md-0 mt-md-5 pr-0 pl-0'>
+                    <div className='name'>
                         <h5>Polytech Grenoble</h5>
                     </div>
                     <div>
                         <h5>{t('livesIn')}</h5>
                     </div>
                     <div>
-                        <Link href="mailto:pro@leojan.fr" target="_blank" rel="noopener noreferrer">
-                            <Button variant="secondary">pro@leojan.fr</Button>
+                        <Link href='mailto:pro@leojan.fr' target='_blank' rel='noopener noreferrer'>
+                            <Button variant='secondary'>pro@leojan.fr</Button>
                         </Link>
                     </div>
                 </Col>
