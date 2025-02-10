a64b54b58a4c0db3f67b0ce7e2b37b773fe0aed0
Leo Jan <leo.jan@doctolib.com>
2025-02-10 20:29:50 +0100

    Remove top level suspense on the main page component, that was blocking the render of all components, hence we had a white blank page every time we were switching the language

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './index.css'
 import './App.css'
-
-import {FC, ReactElement, Suspense} from 'react'
+import {FC} from 'react'
 import Footer from './layouts/footer'
 import {MyNavbar} from './layouts/navbar'
 import AppHeader from './layouts/header/main'
 import {Projects} from './layouts/projects/projects'
 import {Hobbies} from './layouts/hobbies'
 import Travels from './layouts/travels/travelsServer'
-import { Histories } from './layouts/history'
+import {Histories} from './layouts/history'
 
 
 const Page: FC = () => {
@@ -36,17 +35,9 @@ const Page: FC = () => {
     )
 }
 
-// loading component for suspense fallback
-const Loader = (): ReactElement => (
-    <div className="App"/>
-)
-
-// here app catches the suspense from page in case translations are not yet loaded
 const App: FC = () => {
     return (
-        <Suspense fallback={<Loader/>}>
-            <Page/>
-        </Suspense>
+        <Page/>
     )
 }
 export default App
