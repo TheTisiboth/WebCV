670f96ed7a2543a77047d627f18ccbdb28752bb9
Leo Jan <leo.jan@doctolib.com>
2025-02-11 21:31:18 +0100

    Try fiw cv download

--- src/utils/pdf.ts
+++ src/utils/pdf.ts
@@ -1,12 +1,20 @@
 export const handleDownload = async (url: string) => {
     try {
         const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+
         const blob = await response.blob()
         const blobUrl = window.URL.createObjectURL(blob)
-
+        console.log(blobUrl)
         const link = document.createElement('a')
         link.href = blobUrl
+        link.target = '_blank'
+        link.rel = 'noopener noreferrer'
         link.download = url.split('/').pop() as string // Extract filename
+        console.log(link.download)
+        console.log(link)
         document.body.appendChild(link)
         link.click()
         document.body.removeChild(link)
@@ -15,6 +23,7 @@ export const handleDownload = async (url: string) => {
         window.URL.revokeObjectURL(blobUrl)
     } catch (error) {
         console.error('Error downloading file:', error)
+        throw error
     }
 }
 
